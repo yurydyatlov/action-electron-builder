79f3b624b6859bf6e33681fedab5d59a33991f6b
Yura Dyatlov <yuradyatlov@gmail.com>
2024-02-20 12:26:40 +0400

    add force flag

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ const runAction = () => {
 	setEnv("ADBLOCK", true);
 
 	log(`Installing dependencies using ${useNpm ? "NPM" : "Yarn"}…`);
-	run(useNpm ? "npm install" : "yarn", pkgRoot);
+	run(useNpm ? "npm install --force" : "yarn", pkgRoot);
 
 	// Run NPM build script if it exists
 	if (skipBuild) {
